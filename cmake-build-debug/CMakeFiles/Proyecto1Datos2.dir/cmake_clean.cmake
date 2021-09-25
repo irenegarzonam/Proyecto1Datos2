b0256f61d296138a4956f78a0255a3ccb1a1d27c
Irene <igarzona@estudiantec.cr>
2021-09-24 21:43:59 -0600

    server basico y cliente

---
+++ cmake-build-debug/CMakeFiles/Proyecto1Datos2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto1Datos2.dir/holamundoprueba.cpp.obj"
+  "Proyecto1Datos2.exe"
+  "Proyecto1Datos2.exe.manifest"
+  "Proyecto1Datos2.pdb"
+  "libProyecto1Datos2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto1Datos2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Proyecto1Datos2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Proyecto1Datos2.
+# This may be replaced when dependencies are built.
