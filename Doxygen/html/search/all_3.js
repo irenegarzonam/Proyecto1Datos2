a93c6dcd5720bb28986da52c0248648bf76515f7
olman2020 <51057022+olman2020@users.noreply.github.com>
2021-10-01 13:28:05 -0600

    Doxygen

---
+++ Doxygen/html/search/all_3.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['painter_0',['Painter',['../class_painter.html',1,'']]],
+  ['paleta_1',['Paleta',['../class_paleta.html',1,'']]],
+  ['pared_2',['Pared',['../class_pared.html',1,'']]]
+];
---
+++ Doxygen/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "bfjps",
+  1: "bfjps"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "Todo",
+  1: "Clases"
+};
+
